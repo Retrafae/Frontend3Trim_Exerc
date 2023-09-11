5be7a3b17947225ab554bd2e5730b9e60911bc9a
Retrafae <130366200+Retrafae@users.noreply.github.com>
2023-09-11 08:49:39 -0300

    Atualizado aula 2

---
+++ Frontend3Trim_Exerc-main/introducao-javascript/js/principal.js
@@ -0,0 +1,13 @@
+//console.log("O arquivo separado está funcionando");
+
+//------------------------------Daqui pra baixo
+
+//alteração do titulo
+//console.log(document);
+//cria variavel e atribui o valor a ela
+let titulo = document.querySelector(".titulo");
+//console.log(titulo);
+//atribuição de um novo valor na variavel titulo
+titulo.textContent = "Fake Natty Nutrição!";
+
+//alteração da tabela
